c22fe4c60673b7f76293e27b86f06f902340f618
nihat417 <akreminihat@gmail.com>
2024-05-15 14:30:27 +0400

    Added resume

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,21 @@
+// next.config.mjs
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    webpack(config) {
+      config.module.rules.push({
+        test: /\.pdf$/,
+        use: {
+          loader: "file-loader",
+          options: {
+            name: "[name].[ext]",
+            outputPath: "static/pdf/",
+            publicPath: "/_next/static/pdf/",
+          },
+        },
+      });
+      return config;
+    },
+  };
+  
+  export default nextConfig;
+  
--- src/app/common/resume/page.jsx
+++ src/app/common/resume/page.jsx
@@ -1,21 +1,24 @@
-"use client"
+"use client";
 import React, { useState, useEffect } from "react";
 import { Document, Page, pdfjs } from "react-pdf";
-import pdf from "../../../assets/documents/nixatcv.pdf"
+import pdf from "../../../assets/documents/nixatcv.pdf";
+
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const Resume = () => {
-    const [width, setWidth] = useState(1200);
+  const [width, setWidth] = useState(1200);
 
   useEffect(() => {
     setWidth(window.innerWidth);
   }, []);
+
   return (
-    <div>
-        <Document file={pdf} className="d-flex justify-content-center">
-            <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
-        </Document>
+    <div className="flex justify-center">
+      <Document file={pdf}>
+        <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
+      </Document>
     </div>
-  )
-}
+  );
+};
 
 export default Resume;
